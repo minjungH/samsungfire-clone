4e4eac9b65d72e6971abed97d6a3fcf1e74d53cf
minjungH <lunahongmj@gmail.com>
2025-05-29 12:35:22 +0900

    design: main-leftPC화면 영역 작성 완료

---
+++ js/sw-banner.js
@@ -0,0 +1,12 @@
+const swBanner = new Swiper(".sw-banner",{{
+{
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+      navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+}},
+    });
+  </script>
